b3305584ad4194d0972fa63690b72f858863d09d
fatimasaad2003 <s202268600@kfupm.edu.sa>
2025-10-06 11:25:30 +0300

    T2

--- 5-3-react-event-handling/src/components/TaskApp.jsx
+++ 5-3-react-event-handling/src/components/TaskApp.jsx
@@ -11,12 +11,13 @@ export default function TaskApp() {
   const handleDelete = (id) => {
     // TODO: filter tasks by id to 
     // remove the clicked one
-  setTasks((prev) => prev.filter((task) => task.id !== id));
+    setTasks((prev) => prev.filter((task) => task.id !== id));
   };
 
   
   const handleClearAll = () => {
     // TODO: set tasks to empty array
+    setTasks([]);
   };
 
   return (
